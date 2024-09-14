fe12261da382cca4a44224aa6beace198c6efb35
Yugsingh05 <yugsingh7622@gmail.com>
2024-09-14 18:38:53 +0530

    css added

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,7 +1,7 @@
 const conf = {
     appwriteUrl: (import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: (import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: (import.meta.env.VITE_APPWRITE_DATABSE_ID ),
+    appwriteDatabaseId: (import.meta.env.VITE_APPWRITE_DATABASE_ID ),
     appwritePetsCollectionId: (import.meta.env.VITE_APPWRITE_PETS_COLLECTION_ID),
     appwriteBucketId: (import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
