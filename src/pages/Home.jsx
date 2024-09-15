c274a6d5d72e0a9f9170f04b121cdce8ad596208
Yugsingh05 <yugsingh7622@gmail.com>
2024-09-15 09:39:06 +0530

    strory rem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -97,12 +97,6 @@ const Home = () => {
    
 
     <div className="min-h-screen bg-gradient-to-br from-[#AD49E1] to-[#EBD3F8]">
-      <Link 
-        to="/story" 
-        className="inline-block px-4 py-2 bg-white text-[#AD49E1] font-semibold rounded-full shadow-md hover:bg-[#AD49E1] hover:text-white transition duration-300 ease-in-out m-4"
-      >
-        Story
-      </Link>
       {/* Carousel */}
       <div className="relative w-full overflow-hidden h-80 md:h-96 lg:h-[28rem]">
         <div
