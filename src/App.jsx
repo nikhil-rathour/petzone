bfda3c9ca67ebc395ecfdb762925173ebb1f1929
nikhil-rathour <rathournikhil042@gmail.com>
2024-09-15 06:29:32 +0530

    app.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './Components/Header/Header'
+// import Header from './Components/Header/Header'
+import Header from './components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
